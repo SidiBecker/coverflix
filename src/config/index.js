7eb56672692e5eca36fa823788dd96fe75345b74
SidiBecker <sidibecker@hotmail.com>
2022-12-12 11:22:09 -0300

    update dependencies

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const URL_BACKEND = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://coverflix.herokuapp.com';
+const URL_BACKEND = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://coverflix.onrender.com';
 
 export default { URL_BACKEND };
